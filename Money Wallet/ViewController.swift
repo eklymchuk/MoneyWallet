078b31c9f768aaa50f63fed29ab85f789aea7807
arh <arh@arh.ua>
2019-08-08 01:10:34 -0700

    Initial Commit

---
+++ Money Wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Money Wallet
+//
+//  Created by arh on 8/8/19.
+//  Copyright © 2019 arh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
