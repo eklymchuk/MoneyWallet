323acc99f6d337833c3bc295a3bad7f8a4c7b924
arh <arh@arh.ua>
2019-09-11 05:54:05 -0700

    no message

---
+++ Money Wallet/Calculator.swift
@@ -0,0 +1,125 @@
+//
+//  addExpensesViewController.swift
+//  Money Wallet
+//
+//  Created by Profi on 9/7/19.
+//  Copyright © 2019 arh. All rights reserved.
+//
+
+import UIKit
+
+class Calculator: UIViewController {
+    
+    
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+        var category:String = ""
+    
+    //лейбел калькулятора
+    @IBOutlet weak var resultLabel: UILabel!
+    var dotIsPlaced = false
+    var firstNum:Double = 0
+    var secondNum:Double = 0
+    var operationSign: String = ""
+    var stillTyping = false
+    var currentInput: Double {
+        get {
+            return Double(resultLabel.text!)!
+        }
+        set{
+            let value = "\(newValue)"
+            let valueArray = value.components(separatedBy: ".")
+            if valueArray[1] == "0" {
+                resultLabel.text = "\(valueArray[0])"
+            }else{
+                resultLabel.text = "\(newValue)"
+            }
+          
+            stillTyping = false
+        }
+    }
+    
+    @IBAction func digits(_ sender: UIButton) { //цифри
+        let number = sender.currentTitle!
+        
+        if stillTyping {
+            if resultLabel.text!.count < 20 {
+                resultLabel.text = resultLabel.text! + number
+            }
+        }else{
+            resultLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func point(_ sender: UIButton) { //крапка
+        if stillTyping && !dotIsPlaced {
+            resultLabel.text = resultLabel.text! + "."
+            dotIsPlaced = true
+        }else if !stillTyping && !dotIsPlaced {
+            resultLabel.text = "0."
+        }
+    }
+    //опрації з двома операндами
+    func operationWithTwoOperands(operation: (Double, Double) -> Double){
+        currentInput = operation(firstNum, secondNum)
+        stillTyping = false
+    }
+    //кнопки +,-,*,/
+    @IBAction func Buttons(_ sender: UIButton) {
+        operationSign = sender.currentTitle!
+        firstNum = currentInput
+        stillTyping = false
+        dotIsPlaced = false
+    }
+    //кнопка = (а також +,-,*,/)
+    @IBAction func equolityButton(_ sender: UIButton) {
+       
+        if stillTyping {
+            secondNum = currentInput
+        }
+        dotIsPlaced = false
+        
+        switch operationSign {
+        case "+":
+            operationWithTwoOperands{$0 + $1}
+        case "-":
+            operationWithTwoOperands{$0 - $1}
+        case "✕":
+            operationWithTwoOperands{$0 * $1}
+        case "÷":
+            operationWithTwoOperands{$0 / $1}
+        default: break
+        }
+    }
+    //кнопка очищення
+    @IBAction func clearButton(_ sender: UIButton) {
+    
+        firstNum = 0
+        secondNum = 0
+        currentInput = 0
+        dotIsPlaced = false
+        stillTyping = false
+        operationSign = ""
+        resultLabel.text = "0"
+    }
+    //кнопка відсотків
+    @IBAction func percentageButton(_ sender: UIButton) {
+        if firstNum == 0 {
+            currentInput = currentInput / 100
+        }else{
+            secondNum = firstNum * currentInput / 100
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.categoryLabel.text = category
+    }
+    
+}
+
---
+++ Money Wallet/Expenses/ExpensesViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ExpensesViewController.swift
+//  Money Wallet
+//
+//  Created by Profi on 9/8/19.
+//  Copyright © 2019 arh. All rights reserved.
+//
+
+import UIKit
+
+class ExpensesViewController: UIViewController {
+    var model = ExpensesModel()
+
+   
+    @IBAction func addExpenses(_ sender: UIButton) {
+        let category = model.expensesArray[sender.tag]
+        performSegue(withIdentifier: "addExpensesSeag", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addExpensesSeag",
+            let vc = segue.destination as? Calculator {
+            vc.category = sender as! String
+        }
+        
+    }
+}
+
+
+extension ExpensesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return model.expensesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "expenses") as! ExpensesTableViewCell
+        
+        cell.categoryLabel.text = "\(model.expensesArray[indexPath.row].name)"
+        cell.imageView?.image = UIImage(named: model.expensesArray[indexPath.row].image)
+//        cell.addButton.tag = indexPath.row
+        return cell
+    }
+    
+    
+}
+
